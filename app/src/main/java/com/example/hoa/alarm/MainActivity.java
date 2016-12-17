0153b73dc79b4b6ceed5ef5001e5fdb50876f9d7
conghoacntt13t1 <conghoacntt13t1@gmail.com>
2016-12-17 11:32:48 +0700

    first commit

--- app/src/main/java/com/example/hoa/alarm/MainActivity.java
+++ app/src/main/java/com/example/hoa/alarm/MainActivity.java
@@ -2,12 +2,31 @@ package com.example.hoa.alarm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.search_view,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        return true;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+
+        return true;
     }
 }
